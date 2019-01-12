8f48508e9f3f21dcd5b78a6f8d5e3cb0b4e5543a
Emily Lorraine <emilylorraine@Emilys-MacBook-Air.local>
2019-01-12 13:28:01 -0600

    initial commit

---
+++ google/client/build/precache-manifest.3ee031d45a22cf9f4f59014ea40167ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e0a31826a22e39fe541",
+    "url": "/static/css/main.0822591d.chunk.css"
+  },
+  {
+    "revision": "4e0a31826a22e39fe541",
+    "url": "/static/js/main.4e0a3182.chunk.js"
+  },
+  {
+    "revision": "47c911f49308e6d85447",
+    "url": "/static/js/1.47c911f4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0582634ce16bfe190e25bd636d77ce5e",
+    "url": "/index.html"
+  }
+];
